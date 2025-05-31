ee94fd1aef4010525bfedbb2daf126950e7c3670
meethatkar <meethatkar000@gmail.com>
2025-05-31 12:02:16 +0530

    added git ignore and unpushed api key and api for security

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-const BASE_URL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
-const API_KEY = "09d236da4d0c9df6acf2ee5b9de33b5d";
-
 var inputDiv = document.querySelector(".search input");
 var searchBtn = document.querySelector(".search img");
 var weatherImg = document.querySelector("#container>img");
