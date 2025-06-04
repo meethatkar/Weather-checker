ef59878295ba5dca55aff2a934d192ded6185d92
meethatkar <meethatkar000@gmail.com>
2025-06-04 17:13:31 +0530

    corrected logic for blank location name

--- script.js
+++ script.js
@@ -16,42 +16,42 @@ async function getWeatherData(city) {
     const weatherData = await weatherApiData.json();
     // console.log(weatherApiData.status);
     loaderDiv.style.display = "none";
-    if(weatherApiData.status == 404){
+    if (weatherApiData.status == 404) {
         errorDiv.style.display = "block";
-    containerDiv.style.display = "none";
+        containerDiv.style.display = "none";
     }
-    else{
-locDiv.innerText = weatherData.name;
-    tempDiv.innerText = `${weatherData.main.temp}°c`;       //interpolation
-    humidityValDiv.innerText = weatherData.main.humidity + "%";       //concatenation
-    windSpeedValDiv.innerText = weatherData.wind.speed;
-    if (weatherData.weather[0].main == "Clouds") {
-        weatherImg.src = "images/clouds.png";
-    } else if (weatherData.weather[0].main == "Clear") {
-        weatherImg.src = "images/clear.png";
-    }
-    else if (weatherData.weather[0].main == "Rain") {
-        weatherImg.src = "images/rain.png";
-    }
-    else if (weatherData.weather[0].main == "Mist") {
-        weatherImg.src = "images/mist.png";
-    }
-    else if (weatherData.weather[0].main == "Snow") {
-        weatherImg.src = "images/snow.png";
-    }
-    else if (weatherData.weather[0].main == "Humidity") {
-        weatherImg.src = "images/humidity.png";
-    }
-    else if (weatherData.weather[0].main == "Wind") {
-        weatherImg.src = "images/wind.png";
-    }
-    else if (weatherData.weather[0].main == "Drizzle") {
-        weatherImg.src = "images/drizzle.png";
-    }
-    // cardDiv.style.height = "80%";
-    containerDiv.style.display = "flex";
+    else {
+        locDiv.innerText = weatherData.name;
+        tempDiv.innerText = `${weatherData.main.temp}°c`;       //interpolation
+        humidityValDiv.innerText = weatherData.main.humidity + "%";       //concatenation
+        windSpeedValDiv.innerText = weatherData.wind.speed;
+        if (weatherData.weather[0].main == "Clouds") {
+            weatherImg.src = "images/clouds.png";
+        } else if (weatherData.weather[0].main == "Clear") {
+            weatherImg.src = "images/clear.png";
+        }
+        else if (weatherData.weather[0].main == "Rain") {
+            weatherImg.src = "images/rain.png";
+        }
+        else if (weatherData.weather[0].main == "Mist") {
+            weatherImg.src = "images/mist.png";
+        }
+        else if (weatherData.weather[0].main == "Snow") {
+            weatherImg.src = "images/snow.png";
+        }
+        else if (weatherData.weather[0].main == "Humidity") {
+            weatherImg.src = "images/humidity.png";
+        }
+        else if (weatherData.weather[0].main == "Wind") {
+            weatherImg.src = "images/wind.png";
+        }
+        else if (weatherData.weather[0].main == "Drizzle") {
+            weatherImg.src = "images/drizzle.png";
+        }
+        // cardDiv.style.height = "80%";
+        containerDiv.style.display = "flex";
 
-    errorDiv.style.display = "none";
+        errorDiv.style.display = "none";
     }
     // console.log(weatherData);
     // console.log(weatherData.main.temp, " temp");
@@ -61,12 +61,22 @@ locDiv.innerText = weatherData.name;
 }
 
 searchBtn.addEventListener("click", function (elem) {
-    getWeatherData(inputDiv.value);
+    if (/[a-zA-Z0-9]/.test(inputDiv)) {
+        errorDiv.style.display = "flex";
+    }
+    else {
+        getWeatherData(inputDiv.value);
+    }
 });
 
 inputDiv.addEventListener("keyup", function (elem) {
     if (elem.key === 'Enter') {
-        getWeatherData(inputDiv.value);
-        inputDiv.focus();
+        if (/[a-zA-Z0-9]/.test(inputDiv.value)) {
+            errorDiv.style.display = "flex";
+        }
+        else {
+            getWeatherData(inputDiv.value);
+            inputDiv.focus();
+        }
     }
 });
